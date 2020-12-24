6e9d704c731b028c2231a8e48dabc38bec635027
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2020-12-24 14:18:50 +0300

    update rn-starter

---
+++ rn-starter/21 - Screen Boilerplate/App.js
@@ -0,0 +1,26 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/screens/ListScreen';
+import ImageScreen from './src/screens/ImageScreen';
+import CounterScreen from './src/screens/CounterScreen';
+
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreen,
+    Counter: ComponentsScreen
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'App'
+    }
+  }
+);
+
+export default createAppContainer(navigator);
