d22e5ff5b5a311d8fb730030a87a5a64766be4e4
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2020-12-24 22:44:49 +0300

    update rn-starter

---
+++ rn-starter/33 - Creating a Reducer/src/screens/SquareScreen.js
@@ -0,0 +1,60 @@
+import React, { useReducer } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import ColorCounter from '../components/ColorCounter';
+
+
+const COLOR_INCREMENT = 15;
+
+
+const reducer = (state, action) => {
+    // state === { red: number, green: number, blue: number };
+    // action === { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
+
+    switch (action.colorToChange) {
+        case 'red':
+            return { ...state, red: state.red + action.amount };
+        case 'green':
+            return { ...state, green: state.green + action.amount };
+        case 'blue':
+            return { ...state, blue: state.blue + action.amount };
+        default:
+            return state;
+    }
+}
+
+
+const SquareScreen = () => {
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+
+
+    return (
+        <View>
+            <ColorCounter 
+                onIncrease={() => } 
+                onDecrease={() => } 
+                color="Red" 
+            />
+            <ColorCounter 
+                onIncrease={() => } 
+                onDecrease={() => }
+                color="Green" 
+            />
+            <ColorCounter 
+                onIncrease={() => } 
+                onDecrease={() => }
+                color="Blue" 
+            />
+            <View 
+                style={{ 
+                    height: 150, 
+                    width: 150, 
+                    backgroundColor: `rgb(${red},${green},${blue})`
+                }}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
