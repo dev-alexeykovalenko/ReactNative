20729f60d5649ebc9f0099de57bc853cc0e49484
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2020-12-25 22:20:50 +0300

    update - How to Handle Screen Layout

---
+++ rn-starter/46 - AlignItems with Flex/src/screens/BoxScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const BoxScreen = () => {
+    return (
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>Child #1</Text>
+            <Text style={styles.textStyle}>Child #2</Text>
+            <Text style={styles.textStyle}>Child #3</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 1,
+        borderColor: 'black'
+    },
+    textStyle: {
+        alignItems: 'stretch', // flext-start, center, flex-end
+        borderWidth: 3,
+        borderColor: 'red'
+    }
+});
+
+export default BoxScreen;
