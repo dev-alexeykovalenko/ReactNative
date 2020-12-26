bafc6ecb324ffaa1d107df028d28ef93735cecd9
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2020-12-26 13:23:35 +0300

    update - How to Handle Screen Layout

---
+++ rn-starter/49 - Flex Values/src/screens/BoxScreen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const BoxScreen = () => {
+    return (
+        <View style={styles.viewStyle}>
+            <Text style={styles.textOneStyle}>Child #1</Text>
+            <Text style={styles.textTwoStyle}>Child #2</Text>
+            <Text style={styles.textThreeStyle}>Child #3</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 1,
+        borderColor: 'black',
+        height: 200
+    },
+    textOneStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        flex: 4
+    },
+    textTwoStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        flex: 4
+    },
+    textThreeStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        flex: 2
+    }
+});
+
+export default BoxScreen;
