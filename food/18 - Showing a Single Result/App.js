576aeff23be7117250bdfc77c02a245ec5cccbdb
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2020-12-27 23:22:18 +0300

    init - Navigation with Parameters

---
+++ food/18 - Showing a Single Result/App.js
@@ -0,0 +1,19 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import SearchScreen from './src/screens/SearchScreen';
+
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'BusinessSearch',
+    cardStyle: {
+      backgroundColor: 'white'
+    }
+  },
+  
+});
+
+export default createAppContainer(navigator);
