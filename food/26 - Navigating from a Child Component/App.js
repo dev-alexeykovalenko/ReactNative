f812441daf1d398a188c267afa989703ac1c3b60
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2021-01-01 23:33:50 +0300

    update - Navigation with Parameters

---
+++ food/26 - Navigating from a Child Component/App.js
@@ -0,0 +1,21 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import SearchScreen from './src/screens/SearchScreen';
+import ResultsShowScreen from './src/screens/ResultShowScreen';
+
+const navigator = createStackNavigator({
+  Search: SearchScreen,
+  ResultsShow: ResultsShowScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'BusinessSearch',
+    cardStyle: {
+      backgroundColor: 'white'
+    }
+  },
+  
+});
+
+export default createAppContainer(navigator);
