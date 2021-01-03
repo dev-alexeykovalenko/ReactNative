26ff27c768da741ac1bb0bd4c8e0623707fb724b
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2021-01-03 03:45:46 +0300

    update - Advanced State Management with Context

---
+++ blog/13 - Deleting Posts/src/screens/IndexScreen.js
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Context as BlogContext } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
+
+const IndexScreen = () => {
+
+    const { state: blogPosts, addBlogPost } = useContext(BlogContext);
+
+    return (
+        <View>
+            <Button title="Add Post" onPress={addBlogPost} />
+            <FlatList
+                data={state}
+                keyExtractor={(blogPosts) => blogPosts.title}
+                renderItem={({ item}) => {
+                    return (
+                        <View style={styles.rowStyle}>
+                            <Text style={styles.titleStyle}>
+                                {item.title} - {item.id}
+                            </Text>
+                            <TouchableOpacity onPress={() => console.log(item.id)}>
+                                <Feather style={styles.iconStyle} name="trash" />
+                            </TouchableOpacity>
+                        </View>
+                    );
+                }}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    rowStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    titleStyle: {
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 24
+    }
+});
+
+export default IndexScreen;
