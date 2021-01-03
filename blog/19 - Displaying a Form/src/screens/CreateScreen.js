db776f7fd6a49d6aa1038c9712453b5536cdb591
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2021-01-03 05:26:28 +0300

    update - Advanced State Management with Context

---
+++ blog/19 - Displaying a Form/src/screens/CreateScreen.js
@@ -0,0 +1,45 @@
+import React, { useContext, useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { Context as blogContext } from '../context/BlogContext';
+
+const CreateScreen = ({ navigation }) => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    return (
+        <View>
+            <Text style={styles.labelStyle}>Enter Title:</Text>
+            <TextInput
+                style={styles.inputStyle}
+                value={title}
+                onChangeText={(text) => setTitle(text)}
+            />
+            <Text style={styles.labelStyle}>Enter Content:</Text>
+            <TextInput
+                style={styles.inputStyle}
+                value={content}
+                onChangeText={(text) => setContent(text)}
+            />
+            <Button title="Add Blog Post" /> 
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    inputStyle: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5
+    },
+    labelStyle: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5
+    }
+});
+
+export default CreateScreen;
