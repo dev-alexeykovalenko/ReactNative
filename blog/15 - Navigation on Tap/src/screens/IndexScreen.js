7ea10dff6d4b4384a6031c39ebdb825356217996
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2021-01-03 04:12:50 +0300

    update - Advanced State Management with Context

---
+++ blog/15 - Navigation on Tap/src/screens/IndexScreen.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Context as BlogContext } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
+
+const IndexScreen = ({ navigation }) => {
+
+    const { state: blogPosts, addBlogPost, deleteBlogPost } = useContext(BlogContext);
+
+    return (
+        <View>
+            <Button title="Add Post" onPress={addBlogPost} />
+            <FlatList
+                data={blogPosts}
+                keyExtractor={(blogPosts) => blogPosts.title}
+                renderItem={({ item}) => {
+                    return (
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                            <View style={styles.rowStyle}>
+                                <Text style={styles.titleStyle}>
+                                    {item.title} - {item.id}
+                                </Text>
+                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                    <Feather style={styles.iconStyle} name="trash" />
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
+                    );
+                }}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    rowStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    titleStyle: {
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 24
+    }
+});
+
+export default IndexScreen;
