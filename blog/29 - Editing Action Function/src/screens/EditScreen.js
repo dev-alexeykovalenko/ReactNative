95638b029c97c899aa9b8cba26188e5d902a4100
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2021-01-04 22:01:57 +0300

    update - Advanced State Management with Context

---
+++ blog/29 - Editing Action Function/src/screens/EditScreen.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import { Context as blogContext } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
+
+
+const EditScreen = ({ navigation }) => {
+    const id = navigation.getParam('id');
+    const { state: blogPosts, editBlogPost } = useContext(blogContext);
+
+    const blogPost = blogPosts.find(
+        (blogPost) => blogPost.id === id
+    );
+
+    return <BlogPostForm 
+        initialValues={{ 
+            title: blogPost.title, 
+            content: blogPost.content 
+        }}
+        onSubmit={(title, content) => {
+            editBlogPost(id, title, content)
+        }}
+    />;
+};
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
