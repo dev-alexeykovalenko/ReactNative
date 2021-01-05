4cf8db6ac392e3a4c17e01fdbb49af2d3e391632
Alexey Kovalenko <dev.kovalenkoalexey@gmail.com>
2021-01-05 04:24:54 +0300

    update - Data API Sync

---
+++ blog/33 - Remote Fetch of Posts/src/screens/CreateScreen.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { Context as blogContext } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
+
+
+const CreateScreen = ({ navigation }) => {
+    const { addBlogPost } = useContext(blogContext);
+
+    return <BlogPostForm 
+        onSubmit={(title, content) => {
+            addBlogPost(title, content, () => navigation.navigate('Index'));
+        }} 
+    />;
+};
+
+const styles = StyleSheet.create({
+    
+});
+
+export default CreateScreen;
